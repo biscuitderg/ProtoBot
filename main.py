399851384ffc24f0701889fe1cc48fa0a83f1183
biscuitderg <55060780+biscuitderg@users.noreply.github.com>
2019-10-13 18:01:02 -0400

    overhaul of reminders command
    Added separate reminders file to store reminders for if bot goes down, then is restarted. That way reminders will still push out at the right time.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
 import discord
+import asyncio
+from aiofile import AIOFile, LineReader, Writer
+import datetime
 import random
 import time
 from dotenv import load_dotenv
@@ -15,7 +18,41 @@ class CustomClient(discord.Client):
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
+
+    async def add_reminder(self, message, duration, reminder_text=''):
+        async with AIOFile('reminders.txt', 'a+') as reminders:
+            writer = Writer(reminders)
+            if not reminder_text:
+                raw_message = message.content.split(' ')[2:]
+            caller_id = message.author.id
+            channel_id = message.channel.id
+            duration_delta = datetime.timedelta(seconds=duration)
+            reminder_date = datetime.datetime.utcnow() + duration_delta
+            await writer(reminder_date.isoformat() + ' ' + str(channel_id) + ' ' + str(caller_id) + ' ' + reminder_text + '\n')
+
+    async def check_reminders(self):
+        with open('reminders.txt', 'r+') as reminders:
+            lines = reminders.readlines()
+        async with AIOFile('reminders.txt', 'w+') as reminders:
+            writer = Writer(reminders)
+            for line in lines:
+                items = line.strip('\n').split(' ')
+                reminder_date = datetime.datetime.fromisoformat(items[0])
+                channel = self.get_channel(int(items[1]))
+                caller = items[2]
+                to_send = ''
+                if len(items) > 3:
+                    to_send = '<@' + caller + '>: ' + ' '.join(items[3:])
+                if reminder_date < datetime.datetime.utcnow():
+                    if to_send:
+                        await channel.send(to_send)
+                else:
+                    await writer(line)
+
+
     async def on_message(self, message):
+        await self.check_reminders()
+
         if message.author == client.user:
             return
 
@@ -25,25 +62,31 @@ class CustomClient(discord.Client):
         if message.content.lower()[:(len(self.prefix) + 8)] == self.prefix + 'reminder':
             args = message.content.lower()[(len(self.prefix) + 9):].split(' ')
             if args:
-                try:
-                    time.sleep(int(args[0]))
-                except ValueError:
-                    args = ['', 'Invalid arguments given!']
-            if len(args) > 1:
-                await message.channel.send(' '.join(args[1:]))
+                if args[0].isnumeric():
+                    await asyncio.sleep(1)
+                    init_message = 'Sending a reminder in ' + args[0] + ' seconds!'
+                    if len(args) > 1:
+                        reminder_text = ' '.join(args[1:])
+                    else:
+                        reminder_text = ''
+                    await self.add_reminder(message, int(args[0]), reminder_text=reminder_text)
+                    await message.channel.send(init_message)
+                else:
+                    await message.channel.send('Invalid arguments given!')
 
-        if message.content.lower()[:8] == '>pfpwarn' and message.channel.id == 575101474032582676:
+        if message.content.lower().startswith('>pfpwarn') and message.channel.id == 575101474032582676:
             try:
-                user_to_mention = message.content[9:]
+                user_to_mention = message.content.split(' ')[1]
             except IndexError:
                 pass
             else:
                 if user_to_mention[0] != '<':
                     user_to_mention = '<@' + str(user_to_mention) + '>'
                 caller = '<@' + str(message.author.id) + '>'
-                time.sleep(86400)
-                to_send = str(caller) + ', check on ' + str(user_to_mention) + '\'s profile picture!'
-                await message.channel.send(to_send)
+                init_message = 'Sending a reminder for checking on ' + user_to_mention + ' in 24 hours!'
+                await message.channel.send(init_message)
+                to_send = 'Check on ' + str(user_to_mention) + '\'s profile picture!'
+                await self.add_reminder(message, duration=86400, reminder_text=to_send)
 
 
 
